58dc04a7d1b6935c447a538b10c52a228ff17343
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-30 19:09:27 +0800

    update replyMessage

--- app.py
+++ app.py
@@ -225,12 +225,15 @@ def getImageMessage(originalContentUrl):
 
 
 def replyMessage(payload):
-    response = {}
+
+    r = requests.post('https://api.line.me/v2/bot/message/reply', data=json.dumps(payload), headers=HEADER)
+    print(r.content)
     return 'OK'
 
 
 def pushMessage(payload):
-    response = {}
+    r = requests.post('https://api.line.me/v2/bot/message/push', data=json.dumps(payload), headers=HEADER)
+    print(r.content)
     return 'OK'
 
 
