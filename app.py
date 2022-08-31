83b7b834c7d616c8640e0dc144f18c7eacfe765e
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-31 14:17:23 +0800

    add_func_getTodayCovid19Message

--- app.py
+++ app.py
@@ -257,9 +257,11 @@ def getTotalSentMessageCount():
 
 def getTodayCovid19Message():
     r = requests.get("https://covid-19.nchc.org.tw/api/covid19?CK=covid-19@nchc.org.tw&querydata=3001&limited=BGD")
-    date = ""
-    total_count = 0
-    count = 0
+    print(r.json())
+    data = r.json()[0]
+    date = data['a04']
+    total_count = data['a05']
+    count = data['a06']
     return F"日期：{date}, 人數：{count}, 確診總人數：{total_count}"
 
 
