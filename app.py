1a69dea0c32ebeb4c2ca51b924c5da7261b12a15
jasontsaicc <jason.tsaicc@gmail.com>
2022-08-31 18:10:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def index():
                     payload["messages"] = [getTaipei101ImageMessage(),
                                            getTaipei101LocationMessage(),
                                            getMRTVideoMessage(),
-                                           getCallCarMessage()]
+                                           ]
                 elif text == "quoda":
                     payload["messages"] = [
                             {
